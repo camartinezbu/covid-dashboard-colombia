fab0d255c725ad8e5f9c1efb8a06c78a97bbb536
camartinezbu <cambmppbc@gmail.com>
2020-08-06 00:08:40 -0500

    Added Sidebar Panel and Tabs

--- app/ui.R
+++ app/ui.R
@@ -1,68 +1,85 @@
 # User interface using shiny and shinydashboard
 
 ## Header ----
-header <- dashboardHeader(title = "Tablero Coronavirus")
+header <- dashboardHeader(title = "Tablero Coronavirus",
+                          dropdownMenu(type = "messages",
+                                       messageItem(from = "camartinezbu",
+                                                   message = "Haz clic para ver el código fuente.",
+                                                   href = "https://github.com/camartinezbu/covid-dashboard-replica-colombia")))
 
 ## Sidebar ----
-sidebar <- dashboardSidebar(disable = T)
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Panorama", tabName = "overview"),
+    menuItem("Casos", tabName = "cases")
+  )
+)
 
 ## Body ----
 body <- dashboardBody(
   # Value Box row
-  fluidRow(
-    column(width = 3,
-           valueBoxOutput(outputId = "n_confirmed",
-                          width = NULL
-           )
+  tabItems(
+    tabItem(tabName = "overview",
+      fluidRow(
+        column(width = 3,
+               valueBoxOutput(outputId = "n_confirmed",
+                              width = NULL
+               )
+        ),
+        column(width = 3,
+               valueBoxOutput(outputId = "n_active",
+                              width = NULL
+               )
+        ),
+        column(width = 3,
+               valueBoxOutput(outputId = "n_recovered",
+                              width = NULL
+               )
+        ),
+        column(width = 3,
+               valueBoxOutput(outputId = "n_dead",
+                              width = NULL
+               )
+        )
+      ),
+      # Age and Sex composition row
+      fluidRow(
+        column(width = 6,
+               tabBox(title = "Distribución por Edad",
+                      width = NULL,
+                      tabPanel("Confimados",
+                               plotOutput(outputId = "plot_age_total")
+                               ),
+                      tabPanel("Recuperados",
+                               plotOutput(outputId = "plot_age_recovered")
+                               ),
+                      tabPanel("Fallecidos",
+                               plotOutput(outputId = "plot_age_dead")
+                               )
+                      )
+          
+        ),
+        column(width = 6,
+               tabBox(title = "Distribución por Sexo",
+                      width = NULL,
+                      tabPanel("Confimados",
+                               plotOutput(outputId = "plot_sex_total")
+                      ),
+                      tabPanel("Recuperados",
+                               plotOutput(outputId = "plot_sex_recovered")
+                      ),
+                      tabPanel("Fallecidos",
+                               plotOutput(outputId = "plot_sex_dead")
+                      )
+               )
+            
+        )
+      ),
+      paste0("Fecha del reporte: ",
+            format(max(data$`Fecha diagnostico`, na.rm = T), format = "%d/%m/%Y"))
     ),
-    column(width = 3,
-           valueBoxOutput(outputId = "n_active",
-                          width = NULL
-           )
-    ),
-    column(width = 3,
-           valueBoxOutput(outputId = "n_recovered",
-                          width = NULL
-           )
-    ),
-    column(width = 3,
-           valueBoxOutput(outputId = "n_dead",
-                          width = NULL
-           )
-    )
-  ),
-  # Age and Sex composition row
-  fluidRow(
-    column(width = 6,
-           tabBox(title = "Distribución por Edad",
-                  width = NULL,
-                  tabPanel("Confimados",
-                           plotOutput(outputId = "plot_age_total")
-                           ),
-                  tabPanel("Recuperados",
-                           plotOutput(outputId = "plot_age_recovered")
-                           ),
-                  tabPanel("Fallecidos",
-                           plotOutput(outputId = "plot_age_dead")
-                           )
-                  )
-      
-    ),
-    column(width = 6,
-           tabBox(title = "Distribución por Sexo",
-                  width = NULL,
-                  tabPanel("Confimados",
-                           plotOutput(outputId = "plot_sex_total")
-                  ),
-                  tabPanel("Recuperados",
-                           plotOutput(outputId = "plot_sex_recovered")
-                  ),
-                  tabPanel("Fallecidos",
-                           plotOutput(outputId = "plot_sex_dead")
-                  )
-           )
-        
-    )
+    tabItem(tabName = "cases",
+            "Test")
   )
 )
 
